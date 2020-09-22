c63fb981b4cd9b67e2fe491a4b38d1e03400af87
chunkeat99 <65493333+chunkeat99@users.noreply.github.com>
2020-09-22 21:47:24 +0800

    Update main.php

--- templates/main.php
+++ templates/main.php
@@ -51,7 +51,7 @@ html body {
 <div id="container">
 
 	<div style="text-align:center;">
-		<img src="/default.png"/ width="100" height="50">
+		<img src="/default.png"/ width="150" height="100">
 	</div>
 	
 	<?php if(isset($error_msg)){ ?>
